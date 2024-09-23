042bb1e4780f091ce4963ecedadef6f8672df429
Theresa Balsdon <resablsdn@outlook.com>
2024-09-24 01:20:53 +0800

    04
    started on js, stuck on cycling through an array. Taking a break, i bet i am just needing let my brain rest.

---
+++ assets/js/scripts.js
@@ -0,0 +1,36 @@
+
+let currNums = document.getElementById('nums_curr');
+let btnNext = document.getElementById('btn_next');
+
+
+// .push() to add to the display of past numbers 
+// .pop() to put everything back to the beginning?
+
+let groupingHundred = ['123', '456', '789']
+
+
+
+
+function nextSet() {
+  console.log("clicked!")
+  //console.log(groupingHundred.toString());   //123,456,789
+
+//      https://www.freecodecamp.org/news/how-to-loop-through-an-array-in-javascript-js-iterate-tutorial/#:~:text=How%20to%20Loop%20Through%20an%20Array%20with%20a,This%20function%20is%20known%20as%20a%20callback%20function.
+
+}
+
+// when next is clicked
+// update curr numbers to the past number (on the end), but i never want past numbers to clear, just keep adding the curr numbs to the end. Unless the clear button is clicked.
+// clear curr numbers and display the next set of curr numbers
+
+
+// two modes? Grouping and user input? Grouping first.
+
+/*
+let grouping100 = {
+ set00: '123',
+ set01: '456',
+ set02: '789'
+}
+ */
+
